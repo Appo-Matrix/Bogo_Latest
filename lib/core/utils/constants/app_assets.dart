e502a3af46c67ac643ba66d0254ea075601611e5
saddamsheikh01 <xdsheikh776@gmail.com>
2025-09-10 19:56:37 +0500

    changes
    # Conflicts:
    #	lib/core/utils/constants/app_assets.dart

--- lib/core/utils/constants/app_assets.dart
+++ lib/core/utils/constants/app_assets.dart
@@ -2,6 +2,7 @@ class BImages {
   static const String appLogo = "assets/images/app_logo.png";
   static const String appLogoGreen = "assets/images/appLogo.png";
 
+  // Onboarding Gif images
   static const String onboarding1 = "assets/images/onboarding-1.gif";
   static const String onboarding2 = "assets/images/onboarding-2.gif";
   static const String onboarding3 = "assets/images/onboarding-3.gif";
@@ -9,14 +10,16 @@ class BImages {
   static const String onboarding5 = "assets/images/onboarding-5.gif";
   static const String onboarding6 = "assets/images/onboarding-6.gif";
 
+  //Auth social icon
+
   static const String iphone = "assets/icons/iphone_icon.png";
   static const String google = "assets/icons/google_icon.png";
   static const String facebook = "assets/icons/facebook_icon.png";
   static const String person = "assets/icons/person.png";
   static const String lock = "assets/icons/Lock.png";
   static const String phone = "assets/icons/Phone.png";
-  static const String message = "assets/icons/Message.png";
-  static const String card = "assets/icons/Card.png";
+  static const String message= "assets/icons/Message.png";
+  static const String card= "assets/icons/Card.png";
 
   static const String authProfileImage = "assets/icons/profile_image.png";
   static const String authBackground = "assets/images/auth.jpg";
@@ -40,4 +43,8 @@ class BImages {
   static const String emojiFace = "assets/icons/emojiFace.png";
   static const String sendTriangle = "assets/icons/sendTriangle.png";
   static const String phoneMock = "assets/images/phoneMock.png";
+  // filter category images
+
+
+
 }
